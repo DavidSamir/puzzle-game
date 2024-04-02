2bfb0f39cb860afa8a65f6c189e012ffb0a9beb8
David <David.Samir.WD@gmail.com>
2024-04-02 13:50:28 +0400

    add new state to create new tabel

--- backend/db.js
+++ backend/db.js
@@ -1,35 +1,55 @@
-const mysql = require('mysql2');
+const { Pool } = require('pg');
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '12',
-    database: 'puzzle_db'
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'puzzle_db',
+  password: '00',
+  port: 5432, 
 });
 
-connection.connect((err) => {
+pool.connect((err, client, release) => {
   if (err) {
-    console.error('Error connecting to MySQL:', err);
+    console.error('Error connecting to PostgreSQL:', err);
     return;
   }
-  console.log('Connected to MySQL');
-  
-  // Create the sudoku table if it doesn't exist
-  connection.query(`
-    CREATE TABLE IF NOT EXISTS sudoku (
-      id INT AUTO_INCREMENT PRIMARY KEY,
+  console.log('Connected to PostgreSQL');
+
+  // Array of queries to create tables if they do not exist
+  const createTableQueries = [
+    `CREATE TABLE IF NOT EXISTS sudoku (
+      id SERIAL PRIMARY KEY,
       time INT,
       incorrect_cells INT,
       empty_cells INT,
-      date DATETIME,
-      data JSON
-    )`, (err) => {
-    if (err) {
-      console.error('Error creating sudoku table:', err);
-      return;
-    }
-    console.log('Data table created or already exists');
+      difficulty TEXT,
+      date TIMESTAMP,
+      data JSONB
+    )`,
+    `CREATE TABLE IF NOT EXISTS twenty48 (
+      id SERIAL PRIMARY KEY,
+      time INT,
+      difficulty TEXT,
+      score INT,
+      date TIMESTAMP,
+      data JSONB
+    )`
+    // Add more create table queries if needed
+  ];
+
+  // Run each create table query
+  createTableQueries.forEach((query) => {
+    client.query(query, (err, result) => {
+      if (err) {
+        console.error('Error creating table:', err);
+        return;
+      }
+      console.log('Table created or already exists');
+    });
   });
+
+  release(); // Release the client back to the pool
 });
 
-module.exports = connection;
+
+module.exports = pool;
